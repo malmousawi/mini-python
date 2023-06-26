3f022792689ec69319a87051c1a1fabaf4f218d9
mohammad <mohammadalmousawi74@gmail.com>
2023-06-27 00:20:51 +0100

    .

---
+++ pomodoro-gui/main.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+photo = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=photo)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW,)
+label.grid(column=1, row=0)
+
+start_button = Button(text="Start", width=1, highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", width=1, highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check = Label(text="✓", fg=GREEN, bg=YELLOW)
+check.grid(column=1, row=3)
+
+
+window.mainloop()
